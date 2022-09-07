a7360367f46f9af56caa34136846b28c55f20f13
DennisGiardino <brendadennisoviedo@gmail.com>
2022-09-06 22:02:34 -0300

    agergando modificaciones

---
+++ src/Components/Header/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from '../../assets/logo.png'
+import "./NavBar.css"
+
+const NavBar = () => {
+ return(
+ <header>
+    <img src="" alt="" />
+    <h1>Nada Que Ponerme</h1>
+    <nav>
+        <a href="">categoria 1</a>
+        <a href="">categoria 2</a>
+        <a href="">categoreia 3</a>
+        <a href="">categoria 4
+</a>
+    </nav>
+    <img src="" alt="" />
+    
+    </header>
+ )
+}
+export default NavBar 
